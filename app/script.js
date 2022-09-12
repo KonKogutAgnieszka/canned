94cd72a933792c7f923831b9815ef85ae1b1c8af
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2022-09-12 10:23:48 +0200

    style: mobile menu animations, feat: animation toggle

--- app/script.js
+++ app/script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const header = document.querySelector(".header");
+const hamburger = document.querySelector(".header__menu");
+const overlay = document.querySelector(".overlay");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+  overlay.classList.toggle("open");
+  if (overlay.classList.contains("open")) {
+    overlay.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
+  } else {
+    overlay.classList.add("fade-out");
+    overlay.classList.remove("fade-in");
+  }
+});
