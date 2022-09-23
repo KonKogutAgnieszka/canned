f5c217e9f633b02a6ff1d2c3ccd60862ec7935c9
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2022-09-23 13:24:11 +0200

    style: features animation

--- app/script.js
+++ app/script.js
@@ -41,3 +41,19 @@ const crafts = [
   "../images/features/crafts1.jpg",
   "../images/features/crafts2.jpg",
 ];
+
+imgChange = (imgDiv, tab) => {
+  let i = 1;
+  setInterval(() => {
+    if (i < tab.length) {
+      imgDiv.style.backgroundImage = `url(${tab[i]})`;
+      i++;
+    } else {
+      imgDiv.style.backgroundImage = `url(${tab[0]})`;
+      i = 1;
+    }
+  }, 4000);
+};
+
+imgChange(imgDigital, digital);
+imgChange(imgCrafts, crafts);
